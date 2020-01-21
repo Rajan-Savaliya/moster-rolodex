3c8a0607d58f0fb17479b31ca33fc0aee04e0e18
rajan savaliya <rajansavaliya6328@gmail.com>
2020-01-21 21:35:20 +0530

    Updates

---
+++ precache-manifest.75b64082f22475051ee193fb35d799d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0c0e14760c30890fdc39fd1606f1b9",
+    "url": "/moster-rolodex/index.html"
+  },
+  {
+    "revision": "496d1c4ffb5a695dac51",
+    "url": "/moster-rolodex/static/css/main.c811c229.chunk.css"
+  },
+  {
+    "revision": "d706f28003964d78c4b2",
+    "url": "/moster-rolodex/static/js/2.1339efe9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/moster-rolodex/static/js/2.1339efe9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "496d1c4ffb5a695dac51",
+    "url": "/moster-rolodex/static/js/main.d1d5dc82.chunk.js"
+  },
+  {
+    "revision": "5797584c0a72a84d2988",
+    "url": "/moster-rolodex/static/js/runtime-main.3509e36a.js"
+  }
+]);
